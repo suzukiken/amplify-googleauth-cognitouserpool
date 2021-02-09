d21648dd508920bbc6f9beda9a377efecf65d1e2
ken <ken@figment-research.com>
2021-02-09 00:22:58 +0000

    wip

--- src/App.js
+++ src/App.js
@@ -8,9 +8,24 @@ import { DataGrid } from '@material-ui/data-grid'
 
 Amplify.configure(awsExports)
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    // setState を利用する関数はbind する。
+    // これをしないとコードがキモくなる
+    this.get_order = this.get_order.bind(this);
+    this.search_order = this.search_order.bind(this);
+    this.orderIdChanged = this.orderIdChanged.bind(this);
+    this.prefChanged = this.prefChanged.bind(this);
+    this.state = {
+      order_id: 1,
+      search_pref: '県',
+      rows: [],
+      columns: []
+    }
+  }
 
-  async function sign_in() {
+  async sign_in() {
     try {
       await Auth.federatedSignIn()
     }
@@ -22,7 +37,7 @@ function App() {
     }
   }
 
-  async function sign_out() {
+  async sign_out() {
     try {
       await Auth.signOut()
     }
@@ -34,73 +49,105 @@ function App() {
     }
   }
 
-  async function get_order() {
+  orderIdChanged(e) {
+    this.setState({
+      order_id: e.target.value
+    })
+  }
+
+  prefChanged(e) {
+    this.setState({
+      search_pref: e.target.value
+    })
+  }
+
+  async get_order(e) {
+    e.preventDefault()
     try {
-      const query_response = await API.graphql(graphqlOperation(getOrder, { id: 1 }))
+      const query_response = await API.graphql(graphqlOperation(getOrder, { id: this.state.order_id }))
       console.log(query_response.data)
+      let rows = []
+      rows.push(query_response.data.getOrder)
+      this.setState({
+        rows: rows,
+        columns: [
+          { field: 'id', headerName: 'ID' },
+          { field: 'updated', headerName: 'Updated' }
+        ]
+      })
     }
     catch (err) { console.log('error get order') }
   }
 
-  async function search_order() {
+  async search_order(e) {
+    e.preventDefault()
     try {
-      const query_response = await API.graphql(graphqlOperation(searchOrder, { prefecture: "県" }))
+      const query_response = await API.graphql(graphqlOperation(searchOrder, { prefecture: this.state.search_pref }))
       console.log(query_response.data)
+      let rows = []
+      let columns = []
+      let cols = query_response.data.searchOrder.columns
+      let ros = query_response.data.searchOrder.rows
+      for (let i in cols) {
+        columns.push({ field: cols[i].name, headerName: cols[i].name })
+      }
+      for (let i in ros) {
+        let rdata = { id: i }
+        for (let n = 0; n < ros[i].length; n++) {
+          rdata[columns[n].field] = ros[i][n]
+        }
+        rows.push(rdata)
+      }
+      this.setState({
+        rows: rows,
+        columns: columns
+      })
     }
     catch (err) { console.log('error search order') }
   }
 
-  const columns = [
-    { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'firstName', headerName: 'First name', width: 130 },
-    { field: 'lastName', headerName: 'Last name', width: 130 },
-    { field: 'age', headerName: 'Age', type: 'number', width: 90 }
-  ]
-
-  const lines = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 }
-  ]
-
-  return (
-    <div className="App">
-      <Container maxWidth="lg">
-        <AppBar position="static">
-          <Toolbar>
-            <Typography variant="h6">
-              Orders
-            </Typography>
-            <Button color="inherit" onClick={sign_in}>Login</Button>
-            <Button color="inherit" onClick={sign_out}>Logout</Button>
-          </Toolbar>
-        </AppBar>
-        <Grid container spacing={2}>
-          <Grid item xs={12} style={styles.paper}>
-              <form noValidate autoComplete="off">
-                <TextField variant="outlined" label="Order ID" style={styles.textfield} />
-                <Button variant="contained" color="primary" onClick={get_order} style={styles.button}>Get Order</Button>
-              </form>
+  render() {
+    return (
+      <div className="App">
+        <Container maxWidth="lg">
+          <AppBar position="static" style={styles.appbar}>
+            <Toolbar>
+              <Typography variant="h6">
+                Orders
+              </Typography>
+              <Button color="inherit" onClick={this.sign_in}>Login</Button>
+              <Button color="inherit" onClick={this.sign_out}>Logout</Button>
+            </Toolbar>
+          </AppBar>
+          <Grid container spacing={2}>
+            <Grid item xs={12} style={styles.grid}>
+                <form noValidate autoComplete="off" onSubmit={this.get_order}>
+                  <TextField size="small" label="Number" variant="outlined" type="number" onChange={this.orderIdChanged} value={this.state.order_id} style={styles.textfield} />
+                  <Button variant="contained" color="primary" type="submit" style={styles.button}>Get Order</Button>
+                </form>
+            </Grid>
+            <Grid item xs={12} style={styles.grid}>
+                <form noValidate autoComplete="off" onSubmit={this.search_order}>
+                  <TextField size="small" label="Search string" variant="outlined" onChange={this.prefChanged} value={this.state.search_pref} style={styles.textfield} />
+                  <Button variant="contained" color="primary" type="submit" style={styles.button}>Search Order</Button>
+                </form>
+            </Grid>
+            <Grid item xs={12} style={styles.grid}>
+              <DataGrid rows={this.state.rows} columns={this.state.columns} pageSize={10} autoHeight />
+            </Grid>
           </Grid>
-          <Grid item xs={12} style={styles.paper}>
-              <form noValidate autoComplete="off">
-                <TextField variant="outlined" label="Search string in prefecture" style={styles.textfield} />
-                <Button variant="contained" color="primary" onClick={search_order} style={styles.button}>Search Order</Button>
-              </form>
-          </Grid>
-          <Grid item xs={12} style={styles.paper}>
-            <DataGrid rows={lines} columns={columns} pageSize={10} autoHeight />
-          </Grid>
-        </Grid>
-      </Container>
-    </div>
-  );
+        </Container>
+      </div>
+    )
+  }
 }
 
 const styles = {
   header: { padding: '12px' },
-  paper: { padding: '12px' },
+  grid: { padding: '12px' },
   button: { margin: '4px' },
-  textfield: { margin: '4px' }
+  textfield: { margin: '4px' },
+  appbar: { marginBottom: '20px' }
 }
 
 export default App;
